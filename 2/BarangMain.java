b2e0d34d723fb3c32365cbaf8e284e72fd4b9ee5
Diah Putri Nofianti <90998386+Diaputr@users.noreply.github.com>
2022-03-10 22:02:26 +0700

    Update BarangMain.java

--- 2/BarangMain.java
+++ 2/BarangMain.java
@@ -1,15 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package minggu2;
-
-/**
- *
- * @author Diah Putri Nofianti
- */
 public class BarangMain {
     public static void main(String[] args) {
         Barang b1 = new Barang();
