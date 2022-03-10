72c8754403649b7c842a2bc3143941c35d632402
Diah Putri Nofianti <90998386+Diaputr@users.noreply.github.com>
2022-03-10 20:18:51 +0700

    Update Barang.java

--- 2/Barang.java
+++ 2/Barang.java
@@ -1,15 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package minggu2;
-
-/**
- *
- * @author Diah Putri Nofianti
- */
 public class Barang {
     String namaBarang, jenisBarang;
     int stok, hargaSatuan;
