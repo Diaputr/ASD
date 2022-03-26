d8c32c813ff982a2f05a3db8d515dfd67907f584
Diah Putri Nofianti <90998386+Diaputr@users.noreply.github.com>
2022-03-26 10:37:35 +0700

    Add files via upload

---
+++ 6/MainTiket.java
@@ -0,0 +1,32 @@
+package minggu6;
+public class MainTiket {
+    public static void main(String[] abcd) {
+    TiketService pesan = new TiketService();
+    Tiket t1 = new Tiket("Lion Air", 474000, "Bali", "Jakarta");
+    Tiket t2 = new Tiket("Garuda Indonesia", 543000, "Palembang", "Madiun");
+    Tiket t3 = new Tiket("Air Asia", 528000, "Tangerang", "Mataram");
+    Tiket t4 = new Tiket("Ciilink", 649000, "Ambon", "Singapura");
+    Tiket t5 = new Tiket("Batik Air", 453000, "Malang", "Yogyakarta");
+    
+    pesan.tambah(t1);
+    pesan.tambah(t2);
+    pesan.tambah(t3);
+    pesan.tambah(t4);
+    pesan.tambah(t5);
+    
+    System.out.println("=== Tiket pesanan sebelum sorting ===");
+    pesan.tampilAll();
+    
+    System.out.println("=== Tiket pesanan bubblesort harga (asc) ===");
+    pesan.bubbleAsc();
+    pesan.tampilAll();
+    
+    System.out.println("=== Tiket pesanan bubblesort harga (desc) ===");
+    pesan.bubbleDesc();
+    pesan.tampilAll();
+    
+    System.out.println("=== Tiket pesanan sorting harga (asc) ===");
+    pesan.selectionAsc();
+    pesan.tampilAll();
+    }
+}
