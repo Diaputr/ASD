d225ca9736d344c0470401f56186035a716d91ee
Diah Putri Nofianti <90998386+Diaputr@users.noreply.github.com>
2022-05-30 11:25:19 +0700

    Add files via upload

---
+++ 14/Node.java
@@ -0,0 +1,14 @@
+package minggu14;
+public class Node {
+    int data;
+    Node left, right;
+    
+    Node(){
+    }
+    
+    Node(int data){
+        this.left = null;
+        this.data = data;
+        this.right  = null;
+    }
+}
